53ceea2d9b3febb6d296b5be4100aef1c3fc795c
jorgenlampe <47539998+jorgenlampe@users.noreply.github.com>
2026-02-26 16:25:32 +0000

    refactor: reorganize imports and enhance code structure in index.js

--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,4 +1,12 @@
 
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 // Log the codespace name for debugging
 console.log('REACT_APP_CODESPACE_NAME:', process.env.REACT_APP_CODESPACE_NAME);
 
